459b7daf5ecb7222e01bd018855e1c01132867b9
Alexander Legotin <mrproz@yandex.ru>
2022-07-03 20:54:00 +0300

    Added deck preview

---
+++ src/web/src/components/cards/deck-preview.tsx
@@ -0,0 +1,29 @@
+import { Button, Modal, ModalProps } from "react-bootstrap";
+import { addCustomDeck } from "../../features/deck/deckSlice";
+import { useAppDispatch } from "../../store/hooks";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import IDeckCard from "../../types/card.type";
+
+export function DeckPreview({ deckCards }: { deckCards: IDeckCard[] }) {
+  return (
+    <>
+      <div className="container">
+        <h5>Preview</h5>
+        <div className="card-deck">
+          {deckCards.length > 0 &&
+            deckCards.map((card, i) => {
+              return (
+                <div className="col-sm-2">
+                <div className="card text-white bg-info mb-4">
+                  <div className="card-body align-items-center d-flex justify-content-center">
+                    <h5 className="card-title">{card.description}</h5>
+                  </div>
+                </div></div>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/web/src/components/cards/new-deck.tsx
+++ src/web/src/components/cards/new-deck.tsx
@@ -2,16 +2,18 @@ import { Button, Modal, ModalProps } from "react-bootstrap";
 import { addCustomDeck } from "../../features/deck/deckSlice";
 import { useAppDispatch } from "../../store/hooks";
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import IDeckCard from "../../types/card.type";
+import { DeckPreview } from "./deck-preview";
 
 export function NewDeckModal(props: ModalProps) {
   const newDeck = {
-    description: "",
-    cardsString: "",
+    description: "My deck",
+    cardsString: "1,2,3",
   };
 
   const dispatch = useAppDispatch();
+
   const [{ description, cardsString }, setState] = useState(newDeck);
 
   const clearState = () => {
@@ -24,12 +26,10 @@ export function NewDeckModal(props: ModalProps) {
   };
 
   function composeCards(): IDeckCard[] {
-    return cardsString
-      .split(",")
-      .map((item: string) => ({
-        value: item.trim(),
-        description: item.trim(),
-      })) as IDeckCard[];
+    return cardsString.split(",").map((item: string) => ({
+      value: item.trim(),
+      description: item.trim(),
+    })) as IDeckCard[];
   }
 
   function handleSave(modalProps: ModalProps) {
@@ -46,49 +46,57 @@ export function NewDeckModal(props: ModalProps) {
   }
 
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Create custom deck
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Deck name"
-            aria-label="Deck name"
-            name="description"
-            value={description}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Deck cards"
-            aria-label="Deck cards"
-            name="cardsString"
-            value={cardsString}
-            onChange={handleChange}
-          />
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide} variant="secondary">
-          Close
-        </Button>
-        <Button type="submit" onClick={() => handleSave(props)} variant="info">
-          Save
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Create custom deck
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Deck name"
+              aria-label="Deck name"
+              name="description"
+              value={description}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Deck cards"
+              aria-label="Deck cards"
+              name="cardsString"
+              value={cardsString}
+              onChange={handleChange}
+            />
+          </div>
+        </Modal.Body>
+        <DeckPreview deckCards={composeCards()}></DeckPreview>
+
+        <Modal.Footer>
+          <Button onClick={props.onHide} variant="secondary">
+            Close
+          </Button>
+          <Button
+            type="submit"
+            onClick={() => handleSave(props)}
+            variant="info"
+          >
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
